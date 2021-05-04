8d80132bea17fa8661a8b2646791b2c5221fc84d
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-05-04 21:47:28 +0600

    change language and font size

--- components/cityCorporation.js
+++ components/cityCorporation.js
@@ -6,97 +6,97 @@ function cityCorporationTemplate(cityCorporation) {
     return `
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/bcc.png"><img src="cdn/city-corporation/bcc.png" alt="barishal city corporation" class="team-img"></a>
-            <h2>Barishal City Corporation</h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/bcc.png"><img src="cdn/city-corporation/bcc.png" alt="বরিশাল সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>বরিশাল সিটি কর্পোরেশন</h2>
         </div>
     </div>
 
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/ccc.png"><img src="cdn/city-corporation/ccc.png" alt="chittagong city corporation" class="team-img"></a>
-            <h2>Chittagong City Corporation</h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/ccc.png"><img src="cdn/city-corporation/ccc.png" alt="চট্টগ্রাম সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>চট্টগ্রাম সিটি কর্পোরেশন</h2>
         </div>
     </div>
 
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/ccc.jpg"><img src="cdn/city-corporation/ccc.jpg" alt="comilla city corporation" class="team-img"></a>
-            <h2>Comilla City Corporation</h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/ccc.jpg"><img src="cdn/city-corporation/ccc.jpg" alt="কুমিল্লা সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>কুমিল্লা সিটি কর্পোরেশন</h2>
         </div>
     </div>
 
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/dncc.png"><img src="cdn/city-corporation/dncc.png" alt="dhaka north city corporation" class="team-img"></a>
-            <h2>Dhaka North City Corporation</h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/dncc.png"><img src="cdn/city-corporation/dncc.png" alt="ঢাকা-উত্তর সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>ঢাকা-উত্তর সিটি কর্পোরেশন</h2>
         </div>
     </div>
 
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/dscc.png"><img src="cdn/city-corporation/dscc.png" alt="dhaka south city corporation" class="team-img"></a>
-            <h2>Dhaka South City Corporation</h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/dscc.png"><img src="cdn/city-corporation/dscc.png" alt="ঢাকা-দক্ষিণ সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>ঢাকা-দক্ষিণ সিটি কর্পোরেশন</h2>
         </div>
     </div>
 
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/gcc.png"><img src="cdn/city-corporation/gcc.png" alt="gazipur city corporation" class="team-img"></a>
-            <h2>Gazipur City Corporation</h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/gcc.png"><img src="cdn/city-corporation/gcc.png" alt="গাজীপুর সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>গাজীপুর সিটি কর্পোরেশন</h2>
         </div>
     </div>
 
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/ncc.png"><img src="cdn/city-corporation/ncc.png" alt="narayanganj city corporation" class="team-img"></a>
-            <h2>Narayanganj City Corporation</h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/ncc.png"><img src="cdn/city-corporation/ncc.png" alt="নারায়ণগঞ্জ সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>নারায়ণগঞ্জ সিটি কর্পোরেশন</h2>
         </div>
     </div>
 
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/kcc.jpg"><img src="cdn/city-corporation/kcc.jpg" alt="khulna city corporation " class="team-img"></a>
-            <h2>Khulna City Corporation </h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/kcc.jpg"><img src="cdn/city-corporation/kcc.jpg" alt="খুলনা সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>খুলনা সিটি কর্পোরেশন</h2>
         </div>
     </div>
 
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/mcc.png"><img src="cdn/city-corporation/mcc.png" alt="mymensingh city corporation" class="team-img"></a>
-            <h2>Mymensingh City Corporation</h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/mcc.png"><img src="cdn/city-corporation/mcc.png" alt="ময়মনসিংহ সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>ময়মনসিংহ সিটি কর্পোরেশন</h2>
         </div>
     </div>
 
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/rcc.png"><img src="cdn/city-corporation/rcc.png" alt="rajshahi city corporation" class="team-img"></a>
-            <h2>Rajshahi City Corporation</h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/rcc.png"><img src="cdn/city-corporation/rcc.png" alt="রাজশাহী সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>রাজশাহী সিটি কর্পোরেশন</h2>
         </div>
     </div>
 
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/rcc2.png"><img src="cdn/city-corporation/rcc2.png" alt="rangpur city corporation" class="team-img"></a>
-            <h2>Rangpur City Corporation</h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/rcc2.png"><img src="cdn/city-corporation/rcc2.png" alt="রংপুর সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>রংপুর সিটি কর্পোরেশন</h2>
         </div>
     </div>
 
     <!-- city-corporation -->
     <div class="col-md-4">
-        <div class="justify-content-between align-content-between team-card">
-            <a href="cdn/city-corporation/scc.png"><img src="cdn/city-corporation/scc.png" alt="sylhet city corporation" class="team-img"></a>
-            <h2>Sylhet City Corporation</h2>
+        <div class="justify-content-between align-content-between city-corporation">
+            <a href="cdn/city-corporation/scc.png"><img src="cdn/city-corporation/scc.png" alt="সিলেট সিটি কর্পোরেশন" class="city-corporation-img"></a>
+            <h2>সিলেট সিটি কর্পোরেশন</h2>
         </div>
     </div>
     `;
